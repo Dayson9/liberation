6d33b73819f1130998f87d8d23d2bec3b0f71fd5
Dayson9 <ainsteinwells@gmail.com>
2024-07-29 23:18:36 +0100

    update

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.use(express.static('dist'));
+
+app.use(express.raw({ type: '*/*', limit: '1mb' }));
+
+const port = process.env.PORT || 3000;
+const key = process.env.key;
+
+app.listen(port);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+"/dist/home.html");
+});
+
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname+"/dist/home.html");
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname+"/dist/about.html");
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname+"/dist/contact.html");
+});
